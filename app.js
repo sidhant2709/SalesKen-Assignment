a658b2be4d1b69ff8000c7a85814f4940b49f1f3
sidhant <sidhant1593@gmail.com>
2021-06-20 11:54:55 +0530

    stop-music

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const musicContainer = document.querySelector(".music-container");
-var canvas = document.getElementById("mycanvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-console.log(ctx);
 const playBtn = document.querySelector("#play");
 const audio = document.querySelector("#audio");
 const progressContainer = document.querySelector(".progress-container");
@@ -31,20 +30,31 @@ function pauseSong() {
   audio.pause();
 }
 
+let graphValues = [];
+
 function drawGraph() {
-  let graphValues = [
-    -10, 150, 50, 190, 85, 80, 130, 81, 152, 34, 174, 86, 165, 137, 29, 81, 36,
-    54, 61, 29, 81, 61, 44, 96, 40, 87, 67, 138, 75, 65, 93, 86, 56, 68, 35, 41,
-    43, 44, 53, 62, 72, 92, 188, 63, 54, 142, 84, 42, 138, 43, 142, 40, 40, 45,
-    45, 145, 150, 147, 54, 164, 74, 181, 92, 43, 150, 65, 74, 183, 92, 71, 60,
-    188, 148, 146, 145, 46, 43, 42, 52, 145, 46, 147, 45, 143, 42, 41, 42, 40,
-    42, 147, 42, 144, 94, 123, 42, 60, 100, 85, 90, 186, 43, 62, 152, 95, 146,
-    97, 145, 193, 62, 81, 72, 140, 42, 147, 142, 44, 44, 93, 42, 120, 160, 85,
-    90, 76, 42, 44, 44, 43, 42, 60, 60, 65, 90, 96,
-  ];
+  // let graphValues = [
+  //   -10, 150, 50, 190, 85, 80, 130, 81, 152, 34, 174, 86, 165, 137, 29, 81, 36,
+  //   54, 61, 29, 81, 61, 44, 96, 40, 87, 67, 138, 75, 65, 93, 86, 56, 68, 35, 41,
+  //   43, 44, 53, 62, 72, 92, 188, 63, 54, 142, 84, 42, 138, 43, 142, 40, 40, 45,
+  //   45, 145, 150, 147, 54, 164, 74, 181, 92, 43, 150, 65, 74, 183, 92, 71, 60,
+  //   188, 148, 146, 145, 46, 43, 42, 52, 145, 46, 147, 45, 143, 42, 41, 42, 40,
+  //   42, 147, 42, 144, 94, 123, 42, 60, 100, 85, 90, 186, 43, 62, 152, 95, 146,
+  //   97, 145, 193, 62, 81, 72, 140, 42, 147, 142, 44, 44, 93, 42, 120, 160, 85,
+  //   90, 76, 42, 44, 44, 43, 42, 60, 60, 65, 90, 96,
+  // ];
+
+  let graphHeight = [];
+
+  for (let i = 0; i < 133; i++) {
+    graphHeight.push(Math.floor(Math.random() * 100) + 1);
+  }
+
+  graphValues = [-20, ...graphHeight];
+
   ctx.beginPath();
-  ctx.moveTo(6, 150);
-  ctx.lineTo(1000, 150);
+  ctx.moveTo(6, 160);
+  ctx.lineTo(1000, 160);
   ctx.strokeStyle = "grey";
   ctx.stroke();
 
@@ -163,12 +173,11 @@ function drawGraph() {
 
   let width = 3;
   let X = 0;
-  ctx.fillStyle = "rgba(202, 201, 201, 0.3)";
-  console.log(graphValues.length);
+  ctx.fillStyle = "rgba(202, 201, 201, 0.5)";
   for (let i = 0; i < graphValues.length; i++) {
     if (i % 2 === 0) {
       let h = graphValues[i];
-      ctx.fillRect(X, 150 - h / 2, width, h + 10);
+      ctx.fillRect(X, 150 - h / 2, width, h + 20);
     } else {
       let h = graphValues[i];
       ctx.fillRect(X, 150 - h / 2, width, h + 15);
@@ -181,14 +190,12 @@ function drawGraph() {
 drawGraph();
 
 function playSongWithGraph(val) {
-  let graphValues = drawGraph();
-  console.log(graphValues);
   let width = 3;
   let X = 0;
   for (let i = 0; i < graphValues.length; i++) {
     if (i % 2 === 0) {
       let h = graphValues[i];
-      ctx.fillRect(X, 150 - h / 2, width, h + 10);
+      ctx.fillRect(X, 150 - h / 2, width, h + 20);
     } else {
       let h = graphValues[i];
       ctx.fillRect(X, 150 - h / 2, width, h + 15);
@@ -196,14 +203,13 @@ function playSongWithGraph(val) {
     X += width + 3;
     X < val
       ? (ctx.fillStyle = "#E2BFCA")
-      : (ctx.fillStyle = "rgba(202, 201, 201, 0.1)");
+      : (ctx.fillStyle = "rgba(202, 201, 201, 0.5)");
   }
 }
 
 function upadateProgress(e) {
   const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
-  console.log(currentTime * (800 / duration));
   playSongWithGraph(currentTime * (800 / duration));
 }
 
@@ -222,5 +228,14 @@ playBtn.addEventListener("click", () => {
   }
 });
 
+function stopSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  loadSong(songs[songIndex]);
+}
+
 audio.addEventListener("timeupdate", upadateProgress);
 progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", stopSong);
